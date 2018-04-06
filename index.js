4ec8879728d22ff0352661e975f1a6e8b7af4a87
Nicholas D'hondt <nicholas@Nicholass-MacBook-Pro.local>
2018-04-06 15:53:48 +0200

    thalento added endpoint

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 
 // proxy middleware options
 var optionsThalento = {
-        target: 'https://young-retreat-32139.herokuapp.com/signon', // target host
+        target: 'https://one.thalentodev.be/vl/rest/planner', // target host
         changeOrigin: true,               // needed for virtual hosted sites
         ws: true,                         // proxy websockets
         pathRewrite: {
@@ -17,7 +17,22 @@ var optionsThalento = {
 // create the proxy (without context)
 var proxyThalento = proxy(optionsThalento);
 
+
+// proxy middleware options
+var optionsTest = {
+        target: 'https://young-retreat-32139.herokuapp.com/signon', // target host
+        changeOrigin: true,               // needed for virtual hosted sites
+        ws: true,                         // proxy websockets
+        pathRewrite: {
+            '^/test' : '/'           // remove base path
+        }
+    };
+
+// create the proxy (without context)
+var proxyTest= proxy(optionsTest);
+
 // mount `exampleProxy` in web server
 var app = express();
     app.use('/thalento', proxyThalento);
-    app.listen(80);
+    app.use('/test',proxyTest);
+    app.listen(3000);
